3a7ff72c47ae99593f9b67a0da008c2aed7eef2b
Maxwell Kenned <maxwell.kenned@softcomtec.info>
2018-03-21 11:24:41 -0300

    HTACCESS

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('*', (res, req) =>{
-  res.sendFiles(path.join(__dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 const port = process.env.PORT || '8080';
