5ea09d460950f5b65a966a32730f828dd461f072
Maxwell Kenned <maxwell.kenned@softcomtec.info>
2018-03-21 11:16:02 -0300

    HTACCESS

--- app.js
+++ app.js
@@ -8,16 +8,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
-app.all(/.*/, (res, req, next) =>{
-  var host = req.header("host");
-  if (host.match(/^www\..*/i)) {
-    next();
-  } else {
-    res.sendFiles(path.join(__dirname, 'dist/index.html'));
-  }
+app.get('*', (res, req) =>{
+  res.sendFiles(path.join(__dirname, '/dist/index.html'));
 });
 
-const port = process.env.PORT || '3001';
+const port = process.env.PORT || '8080';
 app.set('port', port);
 
 const server = http.createServer(app);
